1372b5c70dfb3bbb9b2236f2ca49743ef200279a
Chereq <mr.dead.snyper@gmail.com>
2015-12-31 21:30:29 +0300

    Update spi.h
    Add FTD2XX FTDI library.

--- src/spi.h
+++ src/spi.h
@@ -1,7 +1,13 @@
+#include "main.h"
+
 #ifndef SPI_H
 #define SPI_H
 
+#ifdef FTD2XX_LIB
+int spi_begin(uint8_t ftdevice);
+#else  // FTD2XX_LIB
 int spi_begin(uint8_t bus, uint8_t port);
+#endif // FTD2XX_LIB
 void spi_end();
 int spi_transfer(uint8_t *bytes, size_t size);
 
