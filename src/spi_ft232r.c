c33c57611b353e0eb94b7a8969d1c26f3819694e
Chereq <mr.dead.snyper@gmail.com>
2015-12-31 21:05:48 +0300

    Update spi_ft232r.c
    Add FTD2XX FTDI library.

--- src/spi_ft232r.c
+++ src/spi_ft232r.c
@@ -3,8 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include "main.h"
+
+#ifdef FTD2XX_LIB
+#include <windows.h>
+#include "ftd2xx.h"
+#else  // FTD2XX_LIB
 #include <ftdi.h>
 #include <libusb.h>
+#endif // FTD2XX_LIB
+
 #include "spi.h"
 
 #define LOW	0
@@ -14,7 +22,8 @@
 #define PIN_FCSN	(1 << 1)	/* RXD */	// nRF2LE1 Chip select
 #define PIN_FMISO	(1 << 2)	/* RTS */
 #define PIN_FMOSI	(1 << 3)	/* CTS */
-#define PIN_FSCK	(1 << 4)	/* DTR */
+//#define PIN_FSCK	(1 << 4)	/* DTR */
+#define PIN_FSCK	(1 << 7)	/* RI */
 #define PIN_RESET	(1 << 5)	/* DSR */	// nRF24LE1 Reset
 #define PIN_PROG	(1 << 6)	/* DCD */	// nRF24LE1 Program
 #define PINS_OUT	(PIN_PROG|PIN_RESET|PIN_FCSN|PIN_FSCK|PIN_FMOSI)
@@ -26,8 +35,12 @@
 #define delay_ms(t) usleep(t * 1000)
 #define delay_s(t) sleep(t)
 
-
+#ifdef FTD2XX_LIB
+FT_HANDLE ftdi_handle;
+DWORD numBytes = 0;
+#else  // FTD2XX_LIB
 static struct ftdi_context *ftdi;
+#endif // FTD2XX_LIB
 static uint8_t pin_state = 0;
 
 static uint8_t digitalWrite(unsigned char pin, int value)
@@ -39,8 +52,13 @@ static uint8_t digitalWrite(unsigned char pin, int value)
 	else
 		pin_state &= ~pin;
 
-	ftdi_write_data(ftdi, &pin_state, sizeof(pin_state));
+    #ifdef FTD2XX_LIB
+    FT_Write(ftdi_handle, &pin_state, sizeof(pin_state), &numBytes);
+    FT_Read(ftdi_handle, &v, sizeof(v), &numBytes);
+    #else  // FTD2XX_LIB
+    ftdi_write_data(ftdi, &pin_state, sizeof(pin_state));
 	ftdi_read_data(ftdi, &v, sizeof(v));
+    #endif // FTD2XX_LIB
 
 	return v;
 }
@@ -62,6 +80,47 @@ static void prog_end()
 	digitalWrite(PIN_RESET, HIGH);
 }
 
+#ifdef FTD2XX_LIB
+int spi_begin(uint8_t ftdevice)
+{
+    int ret;
+
+    if (FT_Open(ftdevice, &ftdi_handle) == FT_OK){
+        printf("ft%d successfully open\n", ftdevice);
+    } else {
+        printf("ft%d open failed\n", ftdevice);
+        return -1;
+    }
+
+    ret = FT_SetBitMode(ftdi_handle, PINS_OUT, FT_BITMODE_SYNC_BITBANG);
+    if (ret != 0) {
+		fprintf(stderr, "unable to set bitmode: %d (%s)\n", ret,
+						FT_W32_GetLastError(ftdi_handle));
+		FT_Close(ftdi_handle);
+		return -5;
+	}
+
+	ret = FT_SetBaudRate(ftdi_handle, FT_BAUD_57600);
+	if (ret != 0) {
+		fprintf(stderr, "unable to set baudrate: %d (%s)\n", ret,
+						FT_W32_GetLastError(ftdi_handle));
+		FT_SetBitMode(ftdi_handle, PINS_OUT, FT_BITMODE_RESET); // disable bitbang?
+		FT_Close(ftdi_handle);
+		return -6;
+	}
+
+	digitalWrite(PIN_PROG, LOW);
+	digitalWrite(PIN_PROG, LOW);
+	digitalWrite(PIN_FSCK, LOW);
+	digitalWrite(PIN_FCSN, HIGH);
+	digitalWrite(PIN_FMOSI, LOW);
+	digitalWrite(PIN_RESET, HIGH);
+
+	prog_begin();
+
+	return 0;
+}
+#else  // FTD2XX_LIB
 int spi_begin(uint8_t bus, uint8_t port)
 {
 	int ret;
@@ -139,13 +198,19 @@ int spi_begin(uint8_t bus, uint8_t port)
 
 	return 0;
 }
+#endif // FTD2XX_LIB
 
 void spi_end()
 {
 	prog_end();
 
+    #ifdef FTD2XX_LIB
+    FT_SetBitMode(ftdi_handle, PINS_OUT, FT_BITMODE_RESET); // disable bitbang?
+	FT_Close(ftdi_handle);
+    #else  // FTD2XX_LIB
 	ftdi_disable_bitbang(ftdi);
 	ftdi_free(ftdi);
+    #endif // FTD2XX_LIB
 }
 
 static int spi_buf_w(const uint8_t *b, size_t s)
@@ -173,10 +238,17 @@ static int spi_buf_w(const uint8_t *b, size_t s)
 			buf[j++] = pin_state;
 		}
 	}
-	j = ftdi_write_data(ftdi, buf, j);
+
+    #ifdef FTD2XX_LIB
+    FT_Write(ftdi_handle, buf, j, &numBytes);
+    free(buf);
+    return numBytes / 8 / BYTES_PER_BIT;
+    #else  // FTD2XX_LIB
+    j = ftdi_write_data(ftdi, buf, j);
 	free(buf);
 
 	return j / 8 / BYTES_PER_BIT;
+    #endif // FTD2XX_LIB
 }
 
 static int spi_buf_r(uint8_t *b, size_t s)
@@ -185,11 +257,21 @@ static int spi_buf_r(uint8_t *b, size_t s)
 	int j = 0, pos;
 	uint8_t *buf = calloc(1, total_size);
 
-	if (ftdi_read_data(ftdi, buf, total_size) != total_size) {
+    #ifdef FTD2XX_LIB
+    FT_Read(ftdi_handle, buf, total_size, &numBytes);
+    if (numBytes != total_size) {
 		fprintf(stderr, "problem reading device\n");
 		free(buf);
 		return 0;
 	}
+    #else  // FTD2XX_LIB
+    if (ftdi_read_data(ftdi, buf, total_size) != total_size) {
+		fprintf(stderr, "problem reading device\n");
+		free(buf);
+		return 0;
+	}
+    #endif // FTD2XX_LIB
+
 
 	for (pos = 0; pos < s; pos++) {
 		uint8_t v = 0;
@@ -229,6 +311,11 @@ int spi_transfer(uint8_t *bytes, size_t size)
 			digitalWrite(PIN_FCSN, HIGH);
 			return pos;
 		}
+		#ifdef FTD2XX_LIB
+        FT_Purge(ftdi_handle, FT_PURGE_RX | FT_PURGE_TX);
+		#else  // FTD2XX_LIB
+		ftdi_usb_purge_buffers(ftdi);
+		#endif // FTD2XX_LIB
 	}
 
 	digitalWrite(PIN_FCSN, HIGH);
